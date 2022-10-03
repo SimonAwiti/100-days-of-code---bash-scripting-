83bcf5c7660631e470ad153f44810fac4a234d1a
SimonAwiti <awitisimon23@gmail.com>
2022-10-03 17:10:41 +0300

    single and multi-line comments in bash scripts

--- script.sh
+++ script.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "This is just the first script"
+
+#This is a one line comment 
+
+# trailing space can be left after the hash
+
+<<comment
+this is a multiline 
+comment 
+that ends here
+
+comment
+
+echo comment 
+
