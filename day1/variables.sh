018965e7b7a0109ca0c78ea9958ccc4ff3927e8f
SimonAwiti <awitisimon23@gmail.com>
2022-10-07 21:30:44 +0300

    day 1 variables

---
+++ day1/variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this is how variables are defined 
+
+<<comment Just to recap on
+multi line commenting 
+comment 
+
+# system variables start with a dollar sign for example
+
+echo $pwd 
+
+echo $bash
+
+#custom variables 
+
+name=simon 
+
+echon $name 
+
